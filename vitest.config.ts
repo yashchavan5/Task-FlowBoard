67b9779c1f57446393e65f0a98b61d0b8f2b8de1
yashchavan5 <yashchavanpatil1@gmail.com>
2026-04-10 19:04:28 +0530

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-})
+  
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/test/setup.ts",
+    include: ["src/**/*.{test,spec}.{ts,tsx}"],
+  },
+});
